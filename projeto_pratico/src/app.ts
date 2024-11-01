01b3536c56ea624ba702412586b4cbc378781b74
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-11-01 17:50:11 -0300

    refactor: Mudança nos nomes dos id buscados para combinarem com o html saindo de camelCase para BEM.

--- projeto_pratico/src/app.ts
+++ projeto_pratico/src/app.ts
@@ -1,3 +1,22 @@
 import { getDefaultProvider, isAddress, formatEther } from "ethers";
 
 const provider = getDefaultProvider("mainnet");
+
+export function initApp() {
+  const walletInput = document.getElementById(
+    "wallet__address"
+  ) as HTMLInputElement;
+  const balanceDisplay = document.getElementById(
+    "balance"
+  ) as HTMLParagraphElement;
+  const transactionsDisplay = document.getElementById(
+    "transactions"
+  ) as HTMLDivElement;
+  const checkBalanceButton = document.getElementById(
+    "check__balance"
+  ) as HTMLButtonElement;
+  const checkTransactionsButton = document.getElementById(
+    "check__transactions"
+  ) as HTMLButtonElement;
+
+}
